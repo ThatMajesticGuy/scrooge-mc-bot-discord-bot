71ccde1b4b76ce799a58b9c6a9ba69c322ac1fef
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-09 16:56:45 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -19,14 +19,14 @@ exports.run = async (bot, message) => {
  const code = args.join(" ");
   try {
       const evaled = client.clean(await eval(code));
-      if(msg.flags.includes("d")) msg.delete();
-      if(msg.flags.includes("s")) return;
-      msg.channel.send(`\`\`\`xl\n${evaled}\n\`\`\``
+      if(message.flags.includes("d")) msg.delete();
+      if(message.flags.includes("s")) return;
+      message.channel.send(`\`\`\`xl\n${evaled}\n\`\`\``
       );
   }
   catch(err) {
-      if(msg.flags[0] && msg.flags[0] === 's')
-        return msg.delete();
+      if(message.flags[0] && message.flags[0] === 's')
+        return message.delete();
       msg.channel.send(`\`ERROR\` \`\`\`xl\n${client.clean(err)}\n\`\`\``);
   }
 };
