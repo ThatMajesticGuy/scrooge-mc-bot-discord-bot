70c302780779f2bd1cc95ef35908937a4bc03b9d
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-09 19:08:52 -0500

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
 var textArray = [
-"You flipped a head 😆"
-"You flipped a tail!🐿️"
+"You flipped a head 😆",
+"You flipped a tail! 🐿️"
 ];
   var hi = Math.floor(Math.random()*textArray.length);
   message.channel.send(`${textArray[hi]}`)
