df3898a4be9b0cd184de0c43207003ca668c6ef0
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-09 19:07:49 -0500

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,17 +1,9 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
-  var embed = new Discord.RichEmbed()
-  .setTitle("Coin Flip:")
-.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Dime_Obverse_13.png/1200px-Dime_Obverse_13.png")
-.addField("You flipped a:", "Head!")
-  var embed2 = new Discord.RichEmbed()
-  .setTitle("Coin Flip:")
-.setThumbnail("https://userscontent2.emaze.com/images/b3d3b3f1-68cc-4a0f-a300-842b3ab09940/ed04e50011223ff55287c77761ada8c1.png")
-.addField("You flipped a:", "Tail!")
 var textArray = [
-message.channel.send({ embed: embed }),
-message.channel.send({ embed: embed2 })
+"You flipped a head 😆"
+"You flipped a tail!🐿️"
 ];
   var hi = Math.floor(Math.random()*textArray.length);
   message.channel.send(`${textArray[hi]}`)
