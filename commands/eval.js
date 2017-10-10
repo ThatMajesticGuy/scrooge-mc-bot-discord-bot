1970e236e8bc1c0f63b2dcab85f3e205811192a3
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-09 19:38:28 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -8,7 +8,6 @@ exports.run = async (bot, message) => {
   .addField("You are restricted from this command", "Its for the bot owner only!")
   const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
   const clean = text => {
-    if(message.author.id !== config.ownerID) return message.channel.send({ embed: embed });
   if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
   else
@@ -17,6 +16,7 @@ exports.run = async (bot, message) => {
   const args = message.content.split(" ").slice(1);
   if (!args) return message.reply("Put what args you want")
     try {
+         if(message.author.id !== config.ownerID) return message.channel.send({ embed: embed });
       const code = args.join(" ");
       let evaled = eval(code);
 
