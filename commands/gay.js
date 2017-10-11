b6a856c9e8a82b5584978d2d28c2de4f37add37c
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-11 17:19:58 -0500

    Create gay.js

---
+++ commands/gay.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+exports.run = (bot, message, args) => {
+let user = message.mentions.users.first()
+if (!user) return message.reply("You need to mention the gay man!")
+var embed = new Discord.RichEmbed()
+.setTitle(":kiss_mm:")
+.setColor("#ff96ea")
+.addField(`Hey everyone! ${message.author.username} is gay with ${user.username}!`)
+message.channel.send({ embed: embed })
