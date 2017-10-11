d24c70621138554d66b9e0b1876a5c00e80a2487
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-10 21:45:31 -0500

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+exports.run = (bot, message, args) => {
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); 
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
+if(commandCheck("/uptime")){
+    var time = process.uptime();
+    var uptime = (time + "").toHHMMSS();
+    message.channel.send(`I have been up for ${uptime}`);
+}
