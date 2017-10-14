03ec3bf0dc8931c2deedb979743e9ed64a230c67
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-13 19:00:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const weather = require('weather-js');
 const fs = require('fs');
 let money = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
 
@@ -115,20 +114,20 @@ bot.on("message", message => {
   try{
       if (!guilds[message.guild.id]) {
           guilds[message.guild.id] = {
-              prefix: "sc!" // default prefix, change it to fit your needs
+              prefix: "sc!" 
           };
       }
   } catch (e) {
     console.log(e);
   }
 
-  if (message.author.bot) return; // ignore any bots
-  const prefix = guilds[message.guild.id].prefix; // multi-guild (will come back to this later)
+  if (message.author.bot) return;
+  const prefix = guilds[message.guild.id].prefix; 
   const args = message.content.split(" ");
   let command = args[0];
   command = command.slice(prefix.length);
-  if(!message.content.startsWith(prefix)) return; // ignore messages without a prefix
-  if(message.channel.type === 'dm') return message.reply("You cant use me in PM."); // prevent commands via dm
+  if(!message.content.startsWith(prefix)) return;
+  if(message.channel.type === 'dm') return message.reply("You cant use me in PM."); 
 
 try {
   let commandFile = require(`./commands/${command}.js`);
@@ -149,36 +148,7 @@ if (command === "setPrefix") {
 }
 
 
-if (command === "weather") {
-weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) { // Make sure you get that args.join part, since it adds everything after weather.
-         if (err) message.channel.send(err);
 
-         // We also want them to know if a place they enter is invalid.
-         if (result.length === 0) {
-             message.channel.send('**Please enter a valid location.**') // This tells them in chat that the place they entered is invalid.
-             return; // This exits the code so the rest doesn't run.
-         }
-
-         // Variables
-         var current = result[0].current; // This is a variable for the current part of the JSON output
-         var location = result[0].location; // This is a variable for the location part of the JSON output
-
-         // Let's use an embed for this.
-         const embed = new Discord.RichEmbed()
-             .setDescription(`**${current.skytext}**`) // This is the text of what the sky looks like, remember you can find all of this on the weather-js npm page.
-             .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
-             .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
-             .setColor(0x00AE86) // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
-             .addField('Timezone',`UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
-             .addField('Degree Type',location.degreetype, true)// This is the field that shows the degree type, and is inline
-             .addField('Temperature',`${current.temperature} Degrees`, true)
-             .addField('Feels Like', `${current.feelslike} Degrees`, true)
-             .addField('Winds',current.winddisplay, true)
-             .addField('Humidity', `${current.humidity}%`, true)
-
-             // Now, let's display it when called
-             message.channel.send({embed});
-           })}
 
            if (command === "help") {
              const prefix = guilds[message.guild.id].prefix;
