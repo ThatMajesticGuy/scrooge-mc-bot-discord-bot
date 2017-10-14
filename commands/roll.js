226ad57a537d8565b663fd61f9602dd60d3ca9d3
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-14 18:42:20 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,7 +1,7 @@
-const Roll = require('roll');
-const roller = new Roll();
+
 const Discord = require('discord.js');
 
-exports.run = function (bot, msg, args) {
+exports.run = function (bot, message, args) {
       var roll = Math.floor(Math.random() * 30) + 1;
     message.reply(" 🎲 You rolled a " + roll);
+};
