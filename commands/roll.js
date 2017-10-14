8f382a1ed38cac8293aa5bdd4b2a0ed2fd08e975
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-14 18:38:41 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -3,26 +3,5 @@ const roller = new Roll();
 const Discord = require('discord.js');
 
 exports.run = function (bot, msg, args) {
-    if (args.length < 1) {
-      msg.channel.send('You must specify in dice notation ([number]d[number])');
-    }
-
-    let reason = '';
-    let footer = '';
-
-    footer += `:game_die: **${args[0]}**`;
-    if (args.length > 1) {
-        reason = args.splice(1).join(' ');
-        footer += ` | ${reason}`;
-    }
-
-    let results = roller.roll(args[0]);
-  if (!results) return msg.channel.send("You must specify in dice notation ([number]d[number])")
-
-
-        `Total: ${results.result}`,
-        `${[].concat.apply([], results.rolled).join(', ').substr(0, 1800)}`,
-
-
-    msg.channel.send(`you rolled a ${results.result}`);
-};
+      var roll = Math.floor(Math.random() * 30) + 1;
+    message.reply(" 🎲 You rolled a " + roll);
