9a2287c3946fa10b6a08627d7c2c725b9299b7a4
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-18 16:24:25 -0500

    Update calculator.js

--- commands/calculator.js
+++ commands/calculator.js
@@ -3,7 +3,7 @@ const stripIndents = require('common-tags').stripIndents;
 const Discord = require('discord.js')
 
 exports.run = (bot, msg) => {
-      var args = message.content.split(' ').slice(1).join(' ');
+      var args = msg.content.split(' ').slice(1).join(' ');
     if (args.length < 1) {
         msg.channel.send('You must provide a equation to be solved on the calculator (Note that multiplication is * and division is /)')
     }
