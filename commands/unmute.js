c4b40d4a524b35cb14270256e233a708897093ea
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-17 20:49:36 -0500

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 
-exports.run = (bot, message) => {
+exports.run = (bot, message, args, channel) => {
   let member = message.mentions.members.first();
   if (!member) return message.reply("Mention someone to unmute them!")
 const muted = message.guild.roles.find('name', 'Muted');
 member.removeRole(muted.id)
 message.channel.send("User has been unmuted!")
 member.send(`You have been unmuted from ***${message.guild.name}*** by ${message.author.name}`)
+  var embed = new Discord.RichEmbed()
+  .setTitle("Unmuted")
+  .setColor("#80d4ff")
+  .setThumbnail(`${member.displayAvatarURL}`)
+  .addField(`${member.username} has been unmuted`, `${member.username} is now free to talk`)
+  channel.send({ embed : embed })
 };
