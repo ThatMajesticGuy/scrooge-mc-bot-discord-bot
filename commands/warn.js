24adb4caaab25fa7945a198fae80399d6a570ede
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-17 20:33:52 -0500

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -9,10 +9,10 @@ if (!user) return message.reply("Mention someone to warn them!")
   if(user.id == config.ownerID) return message.channel.send("You shall not warn the all mighty majestic!")
 message.channel.send(`***${user}*** has been warned!`)
 user.send(`You have been warned in ***${message.guild.name}*** for ***${args}***`)
-const channel = message.guild.channels.find('name', 'mod-log');
-if (!channel) return message.reply("Make a channel called #mod-log!")
 var embed = new Discord.RichEmbed()
 .setTitle("Warn")
+.setColor("#80d4ff")
+.setThumbnail(`${user.displayAvatarURL}`)
 .addField(`${user.tag} has been warned`, `Has been warned by ${message.author.tag}`)
 .addField("User's ID", `${user.id}`)
 channel.send({ embed: embed });
