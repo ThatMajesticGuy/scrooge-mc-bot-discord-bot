bb087cdb18bfe77837e72de1bba8d1f82532831f
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-18 16:26:00 -0500

    Update leet.js

--- commands/leet.js
+++ commands/leet.js
@@ -1,6 +1,7 @@
 const leet = require('leet');
 
-exports.run = (bot, message, args) => {
+exports.run = (bot, message) => {
+    var args = message.content.split(' ').slice(1).join(' ');
   if (args.length < 1) {
       message.channel.send("Put in what text you want to l33tify!");
   }
