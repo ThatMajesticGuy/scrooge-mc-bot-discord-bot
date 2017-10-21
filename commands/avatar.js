b2b5d3425835daba95cdf308cb0da3eb786b976c
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2017-10-21 09:48:17 -0500

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js')
 
 exports.run = async (bot, msg) => {
-    const user = msg.mentions.users.first();
-    msg.reply(user.avatarURL);
+  const user = message.mentions.users.first();
+if (!user) {
+  var embed = new Discord.RichEmbed()
+  .setTitle("Link")
+  .setColor("#4286f4")
+  .setURL(message.author.avatarURL)
+  .setThumbnail(message.author.displayAvatarURL)
+  message.channel.send({ embed: embed })
+} else {
+  var embed2 = new Discord.RichEmbed()
+  .setTitle("Link")
+  .setColor("#4286f4")
+  .setURL(user.avatarURL)
+  .setThumbnail(user.displayAvatarURL)
+  message.channel.send({ embed: embed2 })
 };
